302f147ce6216dd118b6c657768e7c15bc5f85f9
Linda D. Lee <36248152+ldlee3@users.noreply.github.com>
2019-02-10 04:21:08 -0900

    Add files via upload

--- script.js
+++ script.js
@@ -34,3 +34,17 @@ function contribute() {
 	var new_word_def = document.getElementById('textbox3').value;
 	korean_def.push(new_word_def);
 }
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
