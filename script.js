0569d144cef20a54a347f8ab25093231ffef473c
Linda D. Lee <36248152+ldlee3@users.noreply.github.com>
2019-02-09 22:09:49 -0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var korean = [
+	'?? (dang-geun) : of course',
+	'?? (dwen-jang) : whatever'
+]
+
+var mandarin = [
+]
+
+var cantonese = [
+]
+
+var spanish = [
+]
+
+function displayResult() {
+	document.getElementById("text").innerHTML=document.getElementById("textbox1").value;
+} 
