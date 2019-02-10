768668e1ff3a40e01be0e5bdf1658b8d991c8c26
Linda D. Lee <36248152+ldlee3@users.noreply.github.com>
2019-02-10 03:41:22 -0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var korean = [
-	'당근 (dang-geun) : of course',
-	'됀장 (dwen-jang) : whatever'
+var korean_word = [
+	'dang-geun',
+	'dwen-jang'
+]
+var korean_def = [
+	'of course asdlkasjdf asd;lfkjasdf ;sadkdjfj a;sdlkfj asd;lfkj asdf sa;ldkjfsa;lkdj fdsad',
+	'whatever'
 ]
 
 var mandarin = [
@@ -12,6 +16,21 @@ var cantonese = [
 var spanish = [
 ]
 
-function displayResult() {
-	document.getElementById("text").innerHTML=document.getElementById("textbox1").value;
+function displayText() {
+	var word = document.getElementById('resize').value;
+	var def = '';
+	for (i = 0; i < korean_word.length; i++) {
+		if (word == korean_word[i]) {
+			def = korean_def[i];
+			break;
+		}
+	}
+	document.getElementById('definition').innerHTML=def;
 } 
+
+function contribute() {
+	var new_word = document.getElementById('textbox2').value;
+	korean_word.push(new_word);
+	var new_word_def = document.getElementById('textbox3').value;
+	korean_def.push(new_word_def);
+}
